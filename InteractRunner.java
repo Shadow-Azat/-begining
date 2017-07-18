657177c9f424dcdbc845260b419d3f398854375e
Nagimov Azat <azat.nagimov@kidzania.ru>
2017-07-18 12:43:43 +0300

    first calculator

---
+++ InteractRunner.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class InteractRunner {
+
+	public static void main (String args[]) {
+
+		Scanner reader = new Scanner(System.in);
+			
+try{
+			Calculator calc = new Calculator();
+			String exit = "no";
+
+			while(!exit.equals("yes")) {
+			System.out.println("Enter first argument ");
+			String first = reader.next();
+			System.out.println("Enter second argument ");
+			String second = reader.next();
+			calc.add(Integer.valueOf(first), Integer.valueOf(second));
+			System.out.println("\n Result is  " + calc.getResult());
+			calc.cleanResult();
+			System.out.println("Exit? yes / no");
+			exit = reader.next();
+			
+							}
+
+   }
+		
+			finally {
+			reader.close();
+			}		
+
+
+		}
+
+}
+
+
+
+
+				
